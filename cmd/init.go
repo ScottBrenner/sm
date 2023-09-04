560239e31677642c84613f15ec7fcb68e0d9d9ef
Scott Brenner <scott@scottbrenner.me>
2023-09-04 03:19:31 +0000

    init and download

---
+++ cmd/init.go
@@ -0,0 +1,75 @@
+/*
+Copyright © 2023 Scott Brenner <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// initCmd represents the init command
+var initCmd = &cobra.Command{
+	Use:   "init",
+	Short: "Initialize sm-cli functionality",
+	Long: `Initialize sm-cli functionality in a pack's directory.
+
+Prompts for pack's download source and stores it in a text file
+to be referenced for future operations.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		initPackSource()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(initCmd)
+}
+
+func init() {
+	rootCmd.AddCommand(initCmd)
+}
+
+func getPackSource() (downloadURL string, err error) {
+	_, err = fmt.Print("Enter download source for pack: ")
+	if err != nil {
+		return "", err
+	}
+
+	_, err = fmt.Scanln(&downloadURL)
+	if err != nil {
+		return "", err
+	}
+
+	return downloadURL, nil
+}
+
+func setPackSource(downloadURL string) (err error) {
+	f, err := os.Create("source.txt")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	_, err = f.WriteString(downloadURL)
+	if err != nil {
+		return err
+	}
+	f.Sync()
+
+	return nil
+}
+
+func initPackSource() (err error) {
+	sourceURL, err := getPackSource()
+	if err != nil {
+		return err
+	}
+
+	err = setPackSource(sourceURL)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
